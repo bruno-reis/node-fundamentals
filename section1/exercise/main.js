cc888dcba52f2af0bfdbee25554393ae54460816
bruno-reis <bruno-reis@users.noreply.github.com>
2017-07-21 10:39:58 -0300

    added exercise implementation

--- section1/exercise/main.js
+++ section1/exercise/main.js
@@ -2,9 +2,35 @@ const fs = require('fs')
 const request = require('request')
 const prompt = require('prompt')
 
-
 prompt.start()
 
-prompt.get('movie', (err, result) => {
-  console.log('  movie: ' + result.movie);
+prompt.get('character', (err, result) => {
+  search(result.character)
+  logToFile(result.character)
+
+  // const queries = mostSearched(readFileAsArray("results.txt"))
+  // console.log(queries)
 })
+
+
+const search = function (name) {
+  request(`https://swapi.co/api/people/?search=${name}`, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      console.log(JSON.parse(body))
+    }
+  })
+}
+
+const logToFile = function (query) {
+  fs.appendFileSync('results.txt', query + '\n')
+}
+
+const mostSearched = function (queries) {
+  return queries.split("\n").reduce((acc, elem, i, arr) =>
+      (arr.filter(v => v === acc).length >= arr.filter(v => v === elem).length ? acc : elem),
+    null)
+}
+
+const readFileAsArray = function (file) {
+  return fs.readFileSync(file, "utf8").toString()
+}
